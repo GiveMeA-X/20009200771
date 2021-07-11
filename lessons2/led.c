e7b1c172a5536f1dacd4270db901df4dad063c8b
GiveMeA-X <87223144+GiveMeA-X@users.noreply.github.com>
2021-07-11 20:32:10 +0800

    Update led.c

--- lessons2/led.c
+++ lessons2/led.c
@@ -1,1 +1,24 @@
-z
+// C++ code
+//
+int i = 3;
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop()
+{
+  for(i = 3;i<=8;i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(500); // Wait for 1000 millisecond(s)
+    digitalWrite(i, LOW);
+    delay(500); // Wait for 1000 millisecond(s)
+  }
+  
+}
